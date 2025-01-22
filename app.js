99c43d523906ada3d3ff90087a7fd075c2156290
pradeepsingam <ipradeep.here@gmail.com>
2025-01-22 23:31:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const searchbar = document.getElementById('searchbar');
+const searchBtn = document.getElementById('searchicon');
+
+searchBtn.addEventListener('click',()=>{
+    searchbar.style.display = 'flex';
+})
